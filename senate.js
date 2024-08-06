f7a9ada3a50352115773265e6af004dbc972148d
Henryfrank <henryfrank61@yahoo.com>
2024-08-06 23:16:01 +0100

    added some pages

--- senate.js
+++ senate.js
@@ -13,4 +13,35 @@ const toggleMenuClicked = () => {
       closeIcon.style.display = "none";
     }
   };
-  
+
+  function preview_images() {
+    var total_file = document.getElementById("images").files.length;
+    for(var i=0;i<total_file;i++){
+      $('#image_preview').append(`
+                  <div class='col-md-3'>
+                      <img style='width:100%' class='img-responsive' src='${URL.createObjectURL(event.target.files[i])}'>
+                  </div>`);
+    }
+  }
+  function resetForm(){
+    $("#image_preview").html("");
+    return true;
+  }
+
+  let slideIndex = 0;
+showSlides();
+
+// Next-previous control
+function nextSlide() {
+  slideIndex++;
+  showSlides();
+  timer = _timer; // reset timer
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlides();
+  timer = _timer;
+}
+
+// Thumbnail image controlls
